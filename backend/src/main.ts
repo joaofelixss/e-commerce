75627efe2f839834ebf24a2e3a2dad00c46f1725
João Félix <felixjoao651@gmail.com>
2025-05-18 14:48:21 -0400

    teste cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    //app.enableCors(true);
+
+    // ⚠️ CORS configurado corretamente para a Vercel
+    app.enableCors({
+      origin: [
+        'http://localhost:3000', // Para desenvolvimento local
+        'https://e-commerce-frontend.vercel.app', // Substitua pela URL real do seu frontend
+      ],
+      credentials: true, // Deixe true por padrão caso use cookies futuramente
+    });
+
     await app.listen(3000);
-    console.log('Aplicação rodando na porta 3001');
+    console.log('Aplicação rodando na porta 3000');
   } catch (error) {
     console.error('Erro ao iniciar a aplicação:', error);
   }
