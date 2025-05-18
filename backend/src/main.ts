54bc06c5a40ceab1be88a059d6098038f4501f2e
João Félix <felixjoao651@gmail.com>
2025-05-18 14:28:50 -0400

    refactor: CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,12 +5,16 @@ async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-      origin: true,
+      origin: [
+        'https://e-commerce-two-beta-36.vercel.app',
+        'http://localhost:3002',
+      ], // Permite requisições do frontend na porta 3002
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
+      allowedHeaders: 'Content-Type, Accept',
     });
-    await app.listen(process.env.PORT || 3000);
-    console.log(`Aplicação rodando na porta ${process.env.PORT || 3000}`);
+    await app.listen(3000);
+    console.log('Aplicação rodando na porta 3001');
   } catch (error) {
     console.error('Erro ao iniciar a aplicação:', error);
   }
