7a3f840d45f5207095c03fd23261e0701d5b1832
João Félix <felixjoao651@gmail.com>
2025-05-18 14:32:46 -0400

    teste

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,15 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    app.enableCors({
-      origin: [
-        'https://e-commerce-two-beta-36.vercel.app',
-        'http://localhost:3002',
-      ], // Permite requisições do frontend na porta 3002
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
-      allowedHeaders: 'Content-Type, Accept',
-    });
+    app.enableCors();
     await app.listen(3000);
     console.log('Aplicação rodando na porta 3001');
   } catch (error) {
