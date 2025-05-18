a7f8c02d82ea59b32396cf98e52d03bf7488c2c0
João Félix <felixjoao651@gmail.com>
2025-05-18 15:08:03 -0400

    cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,24 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  try {
-    const app = await NestFactory.create(AppModule, {
-      cors: {
-        origin: [
-          'http://localhost:3000',
-          'https://e-commerce-two-beta-36.vercel.app',
-        ],
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-        credentials: true,
-      },
-    });
+  const app = await NestFactory.create(AppModule);
 
-    await app.listen(3000);
-    console.log('Aplicação rodando na porta 3000');
-  } catch (error) {
-    console.error('Erro ao iniciar a aplicação:', error);
-  }
+  app.enableCors({
+    origin: 'https://e-commerce-two-beta-36.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // se precisar usar cookies
+  });
+
+  await app.listen(3000);
+  console.log('Aplicação rodando na porta 3000');
 }
 
 bootstrap();
