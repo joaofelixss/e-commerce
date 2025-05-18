6382f8005df58e1b7ec82c66ae136980370b7822
João Félix <felixjoao651@gmail.com>
2025-05-18 17:17:30 -0400

    REFACTOR: FINALMENTE CORRIGINDO ERRO CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Ativa CORS para qualquer origem (teste apenas)
-  app.enableCors(true);
-
+  app.enableCors({
+    origin: 'https://e-commerce-two-beta-36.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(3000);
   console.log('Aplicação rodando na porta 3000');
 }
