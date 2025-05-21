26059d4c154e75fffa4bf3793e6f41cb9ec32d6d
João Félix <felixjoao651@gmail.com>
2025-05-21 15:17:29 -0400

    refactor: alterando cookies

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -20,7 +20,7 @@ export class AuthController {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'none',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 1000 * 60 * 60 * 24 * 7,
     });
 
